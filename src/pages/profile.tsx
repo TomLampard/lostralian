aa1358666661c6d6b0dc226667481f0f8f024bee
Tom Lampard <t@tomlampard.com>
2023-02-13 11:46:44 +1100

    nothing important

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,24 +1,16 @@
 import { useSession } from "next-auth/react";
-import Head from "next/head";
 import { useRouter } from "next/router";
 import { api } from "../utils/api";
-
-
 
 const UserProfile = () => {
   const { data: sessionData } = useSession();
   const router = useRouter();
   const { data } = api.auth.getUser.useQuery();
 
-
   return (
     <>
-      <Head>
-        <title>{userInformation?.name} | Github</title>
-      </Head>
-      <h1>{userInformation?.name}</h1>
-      <p>{userInformation?.bio}</p>
     </>
   );
 };
-}
+
+export default UserProfile;
